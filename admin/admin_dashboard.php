d43f6378a6f9bbd16a8c839f5ec3cd10b12c5372
jolorambongga <151989553+jolorambongga@users.noreply.github.com>
2024-06-22 21:35:13 +0800

    Add files via upload

--- admin/admin_dashboard.php
+++ admin/admin_dashboard.php
@@ -1,157 +1,29 @@
-
 <?php
 $title = 'Admin - Dashboard';
 $active_dashboard = 'active';
 include_once('header.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>SB Admin 2 - Dashboard</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-</head>
+<body>
 
-<body id="page-top">
-
-<div class="my-wrapper">
+  <div class="my-wrapper">
     <div class="container-fluid">
       <div class="row">
         <div class="col-4">
-          <h1>Dashboard</h1>
+          <h1>Admin Dashboard</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          
         </div>
       </div>
     </div>
-</div>
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-                    <!-- Content Row -->
-                    <div class="row">
+  </div>
 
 
-                    <div class="col-xl-3 col-md-6 mb-4">
-                        <div class="card border-left-primary shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                            New Users (7 days)</div>
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-                                    </div>
-                                    <div class="col-auto">
-                                        <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <div class="col-xl-3 col-md-6 mb-4">
-                      <div class="card border-left-success shadow h-100 py-2">
-                          <div class="card-body">
-                              <div class="row no-gutters align-items-center">
-                                  <div class="col mr-2">
-                                      <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                          Upcoming Appointments
-                                      </div>
-                                      <div class="h5 mb-0 font-weight-bold text-gray-800 accepted-not-past-due-appointments"></div>
-                                  </div>
-                                  <div class="col-auto">
-                                      <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                  </div>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-
- 
-
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Appointments</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800"></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                        <!-- Pending Requests Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Total Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800" id="total-pending-requests"></div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-
-                    <!-- Content Row -->
-                      <div class="row">
-                        <!-- Area Chart -->
-                        <div class="col-xl-8 col-lg-7">
-                            <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Appointment Made (last 30 days)</h6>
-                                </div>
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Pie Chart -->
-                        <div class="col-xl-4 col-lg-5">
-                            <div class="card shadow mb-4">
-                                <!-- Card Header - Dropdown -->
-                                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Appointment per Service</h6>
-                                </div>
-                                <!-- Card Body -->
-                                <div class="card-body">
-                                    <div class="chart-pie pt-4 pb-2">
-                                        <canvas id="myPieChart"></canvas>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                                                                  
-        
-    <script src="script/appointmentgraph.js"></script>
-    <script src="script/piechart.js"></script>
-    <script src="script/upcoming_appointments.js"></script>
-    <script src="script/newusers.js"></script>
-    <script src="script/pending.js"></script>
-    <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'></script>
-
+  <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js'></script>
 </body>
 
 </html>
